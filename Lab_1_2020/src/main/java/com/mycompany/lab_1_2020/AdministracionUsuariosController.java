ae2d648e42ff203b7c807d24256c409b7627cfc0
andres7xd <58347580+andres7xd@users.noreply.github.com>
2020-09-13 12:33:46 -0600

    AdministracionUsuarios
    interface

--- Lab_1_2020/src/main/java/com/mycompany/lab_1_2020/AdministracionUsuariosController.java
+++ Lab_1_2020/src/main/java/com/mycompany/lab_1_2020/AdministracionUsuariosController.java
@@ -7,7 +7,11 @@ package com.mycompany.lab_1_2020;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
@@ -15,6 +19,29 @@ import javafx.fxml.Initializable;
  * @author rache
  */
 public class AdministracionUsuariosController implements Initializable {
+
+    @FXML
+    private Button btnBuscarInformacion;
+    @FXML
+    private TableView<?> tviewInformacion;
+    @FXML
+    private Button btnCerrar;
+    @FXML
+    private Button btnAgregar;
+    @FXML
+    private Button btnBuscarAutorizaciones;
+    @FXML
+    private TableView<?> tviewAutorizaciones;
+    @FXML
+    private Label lblUsuario;
+    @FXML
+    private Label lblCedula;
+    @FXML
+    private Button btnCancelar;
+    @FXML
+    private Button btnLimpiar;
+    @FXML
+    private Button btnGuardar;
 
     /**
      * Initializes the controller class.
