1cbe1c8b49915c0eadc981af5fe0d2ff6b38b57c
luismc1615 <58350003+luismc1615@users.noreply.github.com>
2020-09-15 19:37:52 -0600

    Login interfaz mejorada

--- Laboratorio1Progra3/Laboratorio1/src/main/java/org/una/service/PermisoService.java
+++ Laboratorio1Progra3/Laboratorio1/src/main/java/org/una/service/PermisoService.java
@@ -3,18 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.una.service;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import org.una.laboratorio1.dto.PermisoDTO;
-import org.una.laboratorio1.utils.ConnectionUtils;
+//package org.una.service;
+//
+//import java.io.IOException;
+//import java.util.List;
+//import java.util.concurrent.ExecutionException;
+//import org.una.laboratorio1.dto.AuthenticationRequest;
+//import org.una.laboratorio1.dto.PermisoDTO;
+//import org.una.laboratorio1.dto.UsuarioDTO;
+//import org.una.laboratorio1.utils.ConnectionUtils;
 
 /**
  *
  * @author rache
  */
+<<<<<<< Updated upstream
 public class PermisoService {
     PermisoDTO permiso = new PermisoDTO();
     private final String urlstring = "http://localhost:8099/permisos/";
@@ -45,3 +48,46 @@ public class PermisoService {
 //        return ConnectionUtils.ConnectionToObjectByCedula(urlstringCedula, codigo);
 //    }
 }
+=======
+//public class PermisoService {
+//private final String urlstring = "http://localhost:8099/permisos/";
+//    private PermisoService() {
+//    }
+//
+//    public List<PermisoDTO> getAll() throws InterruptedException, ExecutionException, IOException {
+//        return ConnectionUtils.ListFromConnection(urlstring, PermisoDTO.class);
+//    }
+//
+//    public void add(UsuarioDTO object) throws InterruptedException, ExecutionException, IOException {
+//        ConnectionUtils.ObjectToConnection(urlstring, object);
+//    }
+//
+//    public List<UsuarioDTO> getUserById(Long id) throws IOException {
+//        return ConnectionUtils.ListFromConnection(urlstring, UsuarioDTO.class);
+//    }
+//
+//    public List<UsuarioDTO> finByNombre(String Nombre) throws IOException {
+//        return ConnectionUtils.ConnectionToObjectByField(urlstringNombre, Nombre);
+//    }
+//
+//    public void Login(AuthenticationRequest object) throws InterruptedException, ExecutionException, IOException {
+//        ConnectionUtils.ObjectToConnectionLogin(urlstringLogin, object);
+//
+//    }
+//
+//    public static UsuarioService getInstance() {
+//        return UsuarioServiceHolder.INSTANCE;
+//    }
+//
+//    private static class UsuarioServiceHolder {
+//
+//        private static final UsuarioService INSTANCE = new UsuarioService();
+//    }
+//
+//    public List<UsuarioDTO> finByCedula(String Cedula) throws IOException {
+//        Cedula = datos.getCedula();
+//        return ConnectionUtils.ConnectionToObjectByCedula(urlstringCedula, Cedula);
+//    }
+//
+//}
+>>>>>>> Stashed changes
